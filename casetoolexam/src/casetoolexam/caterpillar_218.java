44912478a64a5b6b5b83354bcfd06b3afaa5236b
Niki218 <nikitacardozo14@gmail.com>
2024-02-10 08:52:22 +0530

    commit

---
+++ casetoolexam/src/casetoolexam/caterpillar_218.java
@@ -0,0 +1,11 @@
+package casetoolexam;
+
+public class caterpillar_218 {
+
+	int moves,cabbages;
+	caterpillar_218 (int moves,int cabbages)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+}
---
+++ casetoolexam/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module casetoolexam {
+}
